0a9e2616aa3a9ae7289c55263123cbac9d64b578
zhanghuan0723 <simon0723@126.com>
2014-12-16 17:40:06 +0800

    add some code

--- src/com/sis/core/fragment/UserCenterTabFragment.java
+++ src/com/sis/core/fragment/UserCenterTabFragment.java
@@ -1,11 +1,16 @@
 package com.sis.core.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sis.core.App;
 import com.sis.core.R;
@@ -15,13 +20,26 @@ import com.sis.core.utils.PreferenceUtils;
 
 public class UserCenterTabFragment extends BaseFragment {
 
-	ImageView mExitIV;
-	TextView userTV;
+	private TextView clearDataTV;
+	private ImageView mExitIV;
+	private TextView userTV;
+	private ProgressBar pb;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View ucLayout = inflater.inflate(R.layout.fragment_user_center, container, false);
 
+		pb = (ProgressBar) ucLayout.findViewById(R.id.pb);
+		clearDataTV = (TextView) ucLayout.findViewById(R.id.clearDataTV);
+		clearDataTV.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				pb.setVisibility(View.VISIBLE);
+				// 利用handler延迟发送更改状态信息
+				handler.sendEmptyMessageDelayed(0, 2000);
+			}
+		});
 		userTV = (TextView) ucLayout.findViewById(R.id.login_account);
 		userTV.setText(App.getPreferenceUtils().getPreferenceStr(PreferenceUtils.KEY_USER_NAME));
 		mExitIV = (ImageView) ucLayout.findViewById(R.id.exit);
@@ -30,12 +48,22 @@ public class UserCenterTabFragment extends BaseFragment {
 			@Override
 			public void onClick(View arg0) {
 				App.getPreferenceUtils().savePreferenceInt(PreferenceUtils.KEY_LOGIN_STATUS, 0);
-				
+
 				forwardActivity(LoginActivity.class);
 				App.getActivityManager().popActivity(getActivity());
 			}
 		});
 		return ucLayout;
 	}
+
+	Handler handler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			pb.setVisibility(View.GONE);
+			Toast.makeText(mActivity, "数据已清除", Toast.LENGTH_SHORT).show();
+		}
+	};
 
 }
